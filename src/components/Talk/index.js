ee3816491ce61ec84cafee7fe4d816557f8e5ad7
Muhammad Talha <talhahanfsd@gmail.com>
2023-03-06 22:18:02 +0500

    Design Lets Talk component

--- src/components/Talk/index.js
+++ src/components/Talk/index.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
 
 const Talk = () => {
   return (
-    <div>Talk</div>
-  )
-}
+    <div className="my-16 px-[1.5rem] md:px-[4rem] lg:px-[6rem] flex flex-col gap-10 xl:max-w-[1440px] xl:mx-auto">
+      <h2 className="font-medium text-[20px] sm:text-[24px] md:text-[30px] lg:text-[36px]">
+        Let talk about your Dream home
+      </h2>
+      <div className="mx-auto w-full">
+        <div className="flex flex-col lg:flex-row justify-between gap-5 md:gap-6 lg:gap-10">
+          <input
+            type="text"
+            placeholder="Name"
+            className="border-b-2 w-full lg:max-w-[35%] p-2"
+          />
+          <input
+            type="text"
+            placeholder="Email/Phone"
+            className="border-b-2 w-full lg:max-w-[35%] p-2 "
+          />
+          <button className="text-[16px] md:text-[20px] font-semibold py-[10px] md:py-[15px] lg:py-[20px] px-[20px] md:px-[30px] lg:px-[50px] bg-purp text-white block mx-auto lg:mx-0">
+            View All
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Talk
+export default Talk;
